b75ac4d209bf81d2780e5e2e9c12522c73898597
sdb4thweb <shankhadeep17.sb@gmail.com>
2021-10-04 13:16:34 +0530

    Hiding API keys

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,15 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
-
+import firebaseSDK from "./firebaseSDK";
 const firebaseConfig = {
-	apiKey: "AIzaSyAYhT2NCCYNFBx0v1kVPpXzClXg_ZSWST0",
-	authDomain: "whatsapp-clone-367fb.firebaseapp.com",
-	projectId: "whatsapp-clone-367fb",
-	storageBucket: "whatsapp-clone-367fb.appspot.com",
-	messagingSenderId: "92007804662",
-	appId: "1:92007804662:web:0287ef111bfd0280ad46d7",
-	measurementId: "G-JEZ76RXCYC",
+	apiKey: firebaseSDK.APIKEY,
+	authDomain: firebaseSDK.AUTHDOMAIN,
+	projectId: firebaseSDK.PROJECTID,
+	storageBucket: firebaseSDK.STORAGEBUCKET,
+	messagingSenderId: firebaseSDK.MESSAGINGSENDERID,
+	appId: firebaseSDK.APPID,
+	measurementId: firebaseSDK.MEASUREMENTID,
 };
 
 const app = initializeApp(firebaseConfig);
